183fba7cfd2b2199306632633cc5fa3db306e6c0
iLLsen <github@illsen.com>
2024-10-09 23:47:37 +0200

    fix comments

--- house_mask_flicker.gd
+++ house_mask_flicker.gd
@@ -1,19 +1,19 @@
 extends Sprite2D
 var _time_passed
-# Randomly update the glow intensity between 0.75 and 0.8
+
 func _ready():
 	randomize()
 	_update_glow_intensity()
-	# Update the glow intensity every 0.5 seconds
 	set_process(true)
 	_time_passed = 0.0
 
 func _process(delta):
 	_time_passed += delta
-	if _time_passed >= 0.07:  # Update every 0.5 seconds
+	if _time_passed >= 0.07:  # Update every 0.07 seconds
 		_update_glow_intensity()
 		_time_passed = 0.0
 
 func _update_glow_intensity():
+	# Randomly update the glow intensity between 0.85 and 0.9
 	var new_intensity = randf_range(0.85, 0.9)
 	material.set("shader_parameter/glow_intensity", new_intensity)
